81224ff4c6c0182d3e48aaa902ee4fdefbda9002
Julian Mutter <julian.mutter@comumail.de>
2024-12-05 23:58:28 +0100

    Re-add library controller functionality

--- backend/api/controllers/library_controller.go
+++ backend/api/controllers/library_controller.go
@@ -2,146 +2,149 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"path"
+	"sort"
+	"strings"
 
+	. "github.com/SheetAble/SheetAble/backend/api/config"
+	"github.com/SheetAble/SheetAble/backend/api/models"
+	"github.com/SheetAble/SheetAble/backend/api/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
 func (server *Server) SyncLibrary(c *gin.Context) {
 	fmt.Printf("Syncing library...\n")
 
-	// libraryPath := path.Join(Config().ConfigPath, "sheets/local-sheets")
+	libraryPath := path.Join(Config().ConfigPath, "sheets/local-sheets")
+	utils.CreateDir(libraryPath)
 
-	// server.SyncSheets(libraryPath)
+	server.SyncSheets(c, libraryPath)
 
-	c.String(http.StatusOK, "Not yet implemented")
+	c.String(http.StatusOK, "Sync successfull")
 }
 
-// func (server *Server) SyncSheets(libraryPath string) {
-// 	localSheets := listSheetsFromFiles(libraryPath)
-// 	dbSheets := models.ListAllSafeSheetNamesAndComposers(server.DB)
+func (server *Server) SyncSheets(c *gin.Context, libraryPath string) {
+	localSheets := listSheetsFromFiles(libraryPath)
+	sort.Slice(localSheets, func(i, j int) bool {
+		return localSheets[i].File < localSheets[j].File
+	})
 
-// 	sort.Slice(localSheets, func(i, j int) bool {
-// 		return models.CompareComposerSheetSafeNames(localSheets[i], localSheets[j]) == -1
-// 	})
-// 	sort.Slice(dbSheets, func(i, j int) bool {
-// 		return models.CompareComposerSheetSafeNames(dbSheets[i], dbSheets[j]) == -1
-// 	})
+	dbSheets, err := ListAllSafeSheetNamesAndComposers(server.DB)
+	if err != nil {
+		utils.DoError(c, http.StatusInternalServerError, fmt.Errorf("unable to retrieve all sheets from db: %v", err.Error()))
+		return
+	}
 
-// 	fmt.Printf("Local sheets: %v\n", localSheets)
-// 	fmt.Printf("Db sheets: %v\n", dbSheets)
+	fmt.Printf("Local sheets: %v\n", localSheets)
+	fmt.Printf("Db sheets: %v\n", dbSheets)
 
-// 	orphanLocalSheets, orphanDbSheets := findOrphansInSortedComposerSheetNames(localSheets, dbSheets)
-// 	sanitizedLocalOrphans := []models.ComposerSheetSafeNames{}
-// 	if len(orphanLocalSheets) != 0 {
-// 		fmt.Printf("Library sync found %v sheets in folder structure but not in database, adding...\n", len(orphanLocalSheets))
-// 		for _, sheet := range orphanLocalSheets {
-// 			// TODO: this assumes file name is already "safe name", therefore make it so!
-// 			if sheet.IsSanitized() {
-// 				fmt.Printf("Saving sheet %v\n", sheet)
-// 				SafeComposer(server, sheet, sheet)
-// 				SafeSheet(server, sheet, sheet)
-// 			} else {
-// 				// Sanitizing file by renaming it
-// 				fmt.Printf("Sanitizing file of %v\n", sheet)
-// 				sanitizedSheet := sheet
-// 				sanitizedSheet.Sanitize()
+	orphanLocalSheets, orphanDbSheets := findOrphansInSortedComposerSheetNames(localSheets, *dbSheets)
+	// Add files to database which are not listed there
+	if len(orphanLocalSheets) != 0 {
+		fmt.Printf("Library sync found %v sheets in folder structure but not in database, adding...\n", len(orphanLocalSheets))
+		for _, sheet := range orphanLocalSheets {
+			sheetUuid, err := GenerateNonexistentSheetUuid(server)
+			if err != nil {
+				utils.DoError(c, http.StatusInternalServerError, fmt.Errorf("unable to check existing uuids: %v", err.Error()))
+				return
+			}
 
-// 				sanitizedLocalOrphans = append(sanitizedLocalOrphans, sanitizedSheet)
+			server.UploadSheet(c, sheet.SheetName, sheetUuid, sheet.ComposerName, sheet.File, false)
+		}
+	}
 
-// 				// Make sure composer dir exists
-// 				utils.CreateDir(sanitizedSheet.ComposerDirPath())
+	// Remove sheets from database which have no local file
+	if len(orphanDbSheets) != 0 {
+		fmt.Printf("Library sync found %v sheets in database but not in local folder structure, removing...\n", len(orphanDbSheets))
+		for _, sheetAndComposer := range orphanDbSheets {
+			err := models.DeleteSheet(server.DB, sheetAndComposer.Uuid)
+			if err != nil {
+				utils.DoError(c, http.StatusInternalServerError, fmt.Errorf("unable to delete sheet: %v", err.Error()))
+				return
+			}
+		}
+	}
+}
 
-// 				// Move sheet file
-// 				e := os.Rename(sheet.SheetPath(), sanitizedSheet.SheetPath())
-// 				if e != nil {
-// 					log.Fatal(e)
-// 				}
+func findOrphansInSortedComposerSheetNames(left, right []ComposerSheetSafeNames) ([]ComposerSheetSafeNames, []ComposerSheetSafeNames) {
+	// TODO: improve speed by using pointers?
+	var orphansLeft, orphansRight []ComposerSheetSafeNames
+	indexLeft, indexRight := 0, 0
+	for indexLeft < len(left) && indexRight < len(right) {
+		if left[indexLeft].File == right[indexRight].File {
+			indexLeft++
+			indexRight++
+		} else if left[indexLeft].File < right[indexRight].File { // left < right -> advance left
+			orphansLeft = append(orphansLeft, left[indexLeft])
+			indexLeft++
+		} else { // right < left -> advance right
+			orphansRight = append(orphansRight, right[indexRight])
+			indexRight++
+		}
+	}
 
-// 				fmt.Printf("Saving sanitized sheet and composer %v\n", sheet)
-// 				SafeComposer(server, sheet, sheet)
-// 				SafeSheet(server, sheet, sanitizedSheet)
-// 			}
+	if indexLeft < len(left) {
+		orphansLeft = append(orphansLeft, left[indexLeft:]...)
+	}
+	if indexRight < len(right) {
+		orphansRight = append(orphansRight, right[indexRight:]...)
+	}
+	return orphansLeft, orphansRight
+}
 
-// 		}
-// 	}
+func listSheetsFromFiles(libraryPath string) []ComposerSheetSafeNames {
+	composerEntries, err := os.ReadDir(libraryPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	// Again check for db Orphans, now against sanitized local orphans
-// 	if len(sanitizedLocalOrphans) != 0 {
-// 		fmt.Printf("Sanitized %v local orphans: checking if still not in db...\n", sanitizedLocalOrphans)
-// 		sort.Slice(sanitizedLocalOrphans, func(i, j int) bool {
-// 			return models.CompareComposerSheetSafeNames(sanitizedLocalOrphans[i], sanitizedLocalOrphans[j]) == -1
-// 		})
+	sheets := []ComposerSheetSafeNames{}
+	for _, composerEntry := range composerEntries {
+		if composerEntry.IsDir() {
+			composer := composerEntry.Name()
 
-// 		_, orphanDbSheets = findOrphansInSortedComposerSheetNames(sanitizedLocalOrphans, orphanDbSheets)
-// 	}
+			sheetEntries, err := os.ReadDir(path.Join(libraryPath, composer))
+			if err != nil {
+				log.Fatal(err)
+			}
 
-// 	if len(orphanDbSheets) != 0 {
-// 		fmt.Printf("Library sync found %v sheets in database but not in local folder structure, removing...\n", len(orphanDbSheets))
-// 		for _, sheetAndComposer := range orphanDbSheets {
-// 			sheet := &models.Sheet{}
-// 			_, err := sheet.DeleteSheet(server.DB, sheetAndComposer.SheetSafeName)
-// 			if err != nil {
-// 				log.Fatal(err)
-// 			}
-// 		}
-// 	}
+			for _, sheetEntry := range sheetEntries {
+				sheet := sheetEntry.Name()
+				if strings.HasSuffix(sheet, ".pdf") {
+					sheetName := strings.TrimSuffix(sheet, ".pdf")
 
-// }
+					file := path.Join(libraryPath, composer, sheet)
 
-// func findOrphansInSortedComposerSheetNames(left, right []models.ComposerSheetSafeNames) ([]models.ComposerSheetSafeNames, []models.ComposerSheetSafeNames) {
-// 	// TODO: improve speed by using pointers?
-// 	var orphansLeft, orphansRight []models.ComposerSheetSafeNames
-// 	indexLeft, indexRight := 0, 0
-// 	for indexLeft < len(left) && indexRight < len(right) {
-// 		compare := models.CompareComposerSheetSafeNames(left[indexLeft], right[indexRight])
-// 		switch compare {
-// 		case -1: // left < right -> advance left
-// 			orphansLeft = append(orphansLeft, left[indexLeft])
-// 			indexLeft++
-// 		case 1: // right < left -> advance right
-// 			orphansRight = append(orphansRight, right[indexRight])
-// 			indexRight++
-// 		default: // both are equal
-// 			indexLeft++
-// 			indexRight++
-// 		}
-// 	}
+					sheets = append(sheets, ComposerSheetSafeNames{File: file, SheetName: sheetName, ComposerName: composer})
+				}
+			}
+		}
+	}
+	return sheets
+}
 
-// 	if indexLeft < len(left) {
-// 		orphansLeft = append(orphansLeft, left[indexLeft:]...)
-// 	}
-// 	if indexRight < len(right) {
-// 		orphansRight = append(orphansRight, right[indexRight:]...)
-// 	}
-// 	return orphansLeft, orphansRight
-// }
+type ComposerSheetSafeNames struct {
+	Uuid         string
+	File         string
+	SheetName    string
+	ComposerName string
+}
 
-// func listSheetsFromFiles(libraryPath string) []models.ComposerSheetSafeNames {
-// 	composerEntries, err := os.ReadDir(libraryPath)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+func ListAllSafeSheetNamesAndComposers(db *gorm.DB) (*[]ComposerSheetSafeNames, error) {
+	var sheets []models.Sheet
+	var toReturn []ComposerSheetSafeNames
+	err := db.Where("was_uploaded = false").Order("file asc").Find(&sheets).Error
+	if err != nil {
+		return &toReturn, err
+	}
 
-// 	sheets := []models.ComposerSheetSafeNames{}
-// 	for _, composerEntry := range composerEntries {
-// 		if composerEntry.IsDir() {
-// 			composer := composerEntry.Name()
+	for _, sheet := range sheets {
+		toReturn = append(toReturn, ComposerSheetSafeNames{Uuid: sheet.Uuid, File: sheet.File})
+	}
 
-// 			sheetEntries, err := os.ReadDir(path.Join(libraryPath, composer))
-// 			if err != nil {
-// 				log.Fatal(err)
-// 			}
-
-// 			for _, sheetEntry := range sheetEntries {
-// 				sheet := sheetEntry.Name()
-// 				if strings.HasSuffix(sheet, ".pdf") {
-// 					sheetName := strings.TrimSuffix(sheet, ".pdf")
-
-// 					sheets = append(sheets, models.ComposerSheetSafeNames{ComposerSafeName: composer, SheetSafeName: sheetName})
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return sheets
-// }
+	return &toReturn, nil
+}
