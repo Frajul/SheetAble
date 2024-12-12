8b8ca9acedbfe2a80ded848e0952a86c6c5c1e4f
Julian Mutter <julian.mutter@comumail.de>
2024-12-12 23:29:33 +0100

    Add docker deploy to flake

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        version = "0.9.0";
         pkgs = nixpkgs.legacyPackages.${system};
         frontend = pkgs.writeShellScriptBin "frontend" ''
           cd frontend
@@ -25,13 +26,37 @@
           cd backend
           ${pkgs.go}/bin/go run .
         '';
+        build_frontend = pkgs.writeShellScriptBin "build_frontend" ''
+          # Builds and embeds frontend into backend
+          cd frontend
+          ${pkgs.nodejs_20}/bin/npm install
+          ${pkgs.nodejs_20}/bin/npm run build
+
+          cd ../backend/api/controllers
+
+          ${pkgs.go-rice}/bin/rice embed-go
+        '';
+        deploy = pkgs.writeShellScriptBin "deploy" ''
+          set -e; set -o pipefail; set -x;
+
+          nix build .#docker
+          image=$((docker load < result) | sed -n '$s/^Loaded image: //p')
+          ${pkgs.docker}/bin/docker image tag "$image" frajul/sheetable:latest
+          ${pkgs.docker}/bin/docker push frajul/sheetable:latest
+        '';
       in
       rec {
         packages.backend = pkgs.buildGoModule {
           pname = "backend";
-          version = "v0.8.1";
+          version = "v${version}";
           src = ./backend;
-          vendorHash = "sha256-6flC+9pTp6cZxFEA7CL704hpL6Ajh63Kg/Fq4XpfFdg=";
+          vendorHash = "sha256-G47jdQCZzDpImPK47YElgJNelzFlWtIZoiRb23iSn6c=";
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            vips
+          ];
         };
         apps.frontend = {
           type = "app";
@@ -42,6 +67,8 @@
           buildInputs = with pkgs; [
             frontend
             backend
+            build_frontend
+            deploy
 
             go
             godef # development only
@@ -55,14 +82,30 @@
             vips
           ];
 
-          # shellHook = ''
-          # '';
-
-          # envvars
-          # DEV=1;
         };
 
         packages.default = packages.backend;
+
+        packages.docker = pkgs.dockerTools.buildImage {
+          name = "sheetable";
+          tag = "latest";
+
+          copyToRoot = with pkgs; [
+            dockerTools.usrBinEnv
+            dockerTools.binSh
+            dockerTools.caCertificates
+
+            coreutils
+            packages.backend
+          ];
+
+          config = {
+            Cmd = [ "/bin/backend" ];
+            Expose = "8080";
+          };
+          created = "now";
+        };
+
       }
     );
 }
